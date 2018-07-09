0576647d187e8381bab3a54871bf5a5f8d49c5f0
Uma Desai <umadesai38@gmail.com>
2018-07-09 23:20:49 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,3 +1,12 @@
 def dance_instructions
-  puts "
+  puts "Plant legs far apart"
+  puts "Bend knees slightly"
+  puts "keep posture loose"
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip step backward"
+  puts "Lower left foot to the ground"
+  puts "Bounce left foot back up slightly, kicking it a inches back"
+  puts "Left, Right, Left and Left"
+  puts
 # Code your methods below
