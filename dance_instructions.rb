19742fc626ac0f7648eedfa837e164fcea3d9081
Uma Desai <umadesai38@gmail.com>
2018-07-09 23:25:20 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -8,5 +8,17 @@ def dance_instructions
   puts "Lower left foot to the ground"
   puts "Bounce left foot back up slightly, kicking it a inches back"
   puts "Left, Right, Left and Left"
-  puts
+  puts "Lower right foot to the ground"
+  puts "Bounce right foor back up slightly, kicking it a few inches back"
+  puts "Right, Left, Right and Right"
+  puts "Holding your arms out in front of you, straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
+  puts" Lift your arms up and down in a loose bouncing movement, in time with the beat of the song. Repeat eight times"
+  puts "Start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left"
+  puts "Lift right arm up and point right elbow diagonally to the right"
+  puts "Raise right forearm so that it points straight up and whip it in small circles to the beat of the song."
+  puts "Do it as if you were a cowboy holding a rope lasso. Repeat eight times"
+end 
+
+
 # Code your methods below
