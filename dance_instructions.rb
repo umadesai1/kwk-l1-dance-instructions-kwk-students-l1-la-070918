daecdcbc2f98e670f4854cbc5a12682e322c0d17
Uma Desai <umadesai38@gmail.com>
2018-07-09 23:28:41 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,4 +1,12 @@
 def starting_stance
-  
+  puts "Plant legs far apart, bend knees slightly and keep posture loose."
+end
+
+def lower_body
+  puts "Lift right foot"
+  puts "Return right foot to the ground"
+  puts "Finishing with a small skip-step backward"
+end
+
 
 # Code your methods below
